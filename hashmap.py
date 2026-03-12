ad8e899f028855eff5b7f14651fc536f8fd170bf
rathinila2006-rn <rathinila2006@gmail.com>
2026-03-12 23:11:33 +0530

    solved(706.design hashmap) leetcode sum

---
+++ hashmap.py
@@ -0,0 +1,23 @@
+class MyHashMap:
+    DEFAULT_SIZE = 1000001
+    def __init__(self):
+        self.map=[0]*self.DEFAULT_SIZE
+        self.present=[False]*self.DEFAULT_SIZE
+    def put(self, key, value):
+        self.map[key] = value
+        self.present[key]=True
+    def get(self, key):
+        if self.present[key]:
+            return self.map[key]
+        else:
+            return -1    
+    def remove(self, key):
+        self.present[key] = False
+     
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
